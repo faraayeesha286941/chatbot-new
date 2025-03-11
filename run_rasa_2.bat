fe5d68457b223740b5e9a53ff6ead04967eda0e0
Fara Ayeesha Binti Ahmad Yusni <92418505+faraayeesha286941@users.noreply.github.com>
2025-03-11 11:41:53 +0800

    Working

---
+++ run_rasa_2.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: Check if the script is running as administrator
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
+    exit /b
+)
+
+:: Change to drive D:
+D:
+
+:: Navigate to the directory
+cd D:\CelcomDigi\chatbot-rasa
+
+:: Activate virtual environment
+call venv\Scripts\activate
+
+:: Run the Rasa actions server
+rasa run --cors "*" --enable-api --port 5005
